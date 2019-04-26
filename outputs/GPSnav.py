06c8200ba89f4ccbe60d0edf6e87f350a52fa271
Brendan <2bndy5@gmail.com>
2019-04-26 00:52:36 -0700

    tested getNewHeading()

--- outputs/GPSnav.py
+++ outputs/GPSnav.py
@@ -1,23 +1,26 @@
 import math
 
 class GPSnav:
-    def __init__(self, driveT, gpsIn):
-        self.waypoints = []
-        self.d = driveT
-        self.gps = gpsIn
+    # def __init__(self, driveT, gpsIn):
+    def __init__(self):
+        self.waypoints = [{'lat': -122.07071872, 'lng': 37.96668393}, {'lat': -122.0711613, 'lng': 37.966604}]
+        # self.d = driveT
+        # self.gps = gpsIn
     
-    def getNewHeading(self):
-        if len(self.waypoints) == 0: return 0
+    def getNewHeading(self, base = 0):
+        if len(self.waypoints) <= (base + 1): return 0
         else: # calc slope between 2 points and return as heading
-            myloc = self.gps.getData()
-            x1 = float(myloc['lat'])
-            x2 = float(self.waypoints[0]['lat'])
-            y1 = float(myloc['lng'])
-            y2 = float(self.waypoints[0]['lng'])
-            m =  (y2 - y1) / (x2 - x1)
-            return math.degrees(math.tan(m))
+            y2 = float(self.waypoints[base + 1]['lat'])
+            x2 = float(self.waypoints[base + 1]['lng'])
+            y1 = float(self.waypoints[base]['lat'])
+            x1 = float(self.waypoints[base]['lng'])
+            heading = math.degrees(math.atan2((y2 - y1), (x2 - x1)))
+            if y1 < 0:
+                return heading * -1
+            else: return heading
 
 
 # self executable loop
 if __name__ == "__main__":
-    pass
+    nav = GPSnav()
+    print(nav.getNewHeading())
