501290c11afa628e9479765bd77c9308c1167843
Brendan <2bndy5@gmail.com>
2019-04-27 21:14:18 -0700

    removed '* -1' on heading calc; compass goes CW

--- outputs/GPSnav.py
+++ outputs/GPSnav.py
@@ -36,9 +36,7 @@ class GPSnav:
             y1 = float(self.waypoints[base]['lat'])
             x1 = float(self.waypoints[base]['lng'])
             heading = math.degrees(math.atan2((y2 - y1), (x2 - x1)))
-            if y1 < 0:
-                return heading * -1
-            else: return heading
+            return heading
 
     def alignHeading(self, heading):
         if abs(heading - gps.heading) < abs(heading + 360 - gps.heading):
