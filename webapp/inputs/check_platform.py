6c5e8c6fccde23983eac71f7c70c021632bf7c67
Tejas Shah <tejashah88@gmail.com>
2019-09-21 09:16:00 -0700

    check_platform not returning early if system isn't raspi

--- webapp/inputs/check_platform.py
+++ webapp/inputs/check_platform.py
@@ -5,13 +5,13 @@
 """
 import io
 
+
 def is_on_raspberry_pi(raise_on_errors=False):
     """ Use this function to check if host machine is Raspberry Pi.
 
     :param bool raise_on_errors: A debugging parameter for unit testing.
 
     """
-    result = False
     try:
         with io.open('/proc/cpuinfo', 'r') as cpuinfo:
             found = False
@@ -22,20 +22,19 @@ def is_on_raspberry_pi(raise_on_errors=False):
                     value = value.strip()
                     # Currently, Broadcomm has an exclusive contract w/ the
                     # Raspberry Pi Foundation
-                    if value.startswith('BCM'): # for future compatibility
+                    if value.startswith('BCM'):  # for future compatibility
                         # not in ('BCM2708', 'BCM2709', 'BCM2835', 'BCM2836'):
                         if not raise_on_errors:
-                            result = False
+                            return False
                         raise ValueError('This system does not appear to be a '
                                          'Raspberry Pi.')
             if not found:
                 if not raise_on_errors:
-                    result = False
+                    return False
                 raise ValueError('Unable to determine if this system is a '
                                  'Raspberry Pi.')
-            result = True
     except IOError:
         if not raise_on_errors:
             return False
         raise ValueError('Unable to open `/proc/cpuinfo`.')
-    return result
+    return True
