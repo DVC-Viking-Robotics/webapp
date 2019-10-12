bd9da149ef14c7994a4085f36f8922178304300d
Tejas Shah <tejashah88@gmail.com>
2019-10-12 11:50:24 -0700

    made ID as indexable key

--- webapp/users.py
+++ webapp/users.py
@@ -18,9 +18,9 @@ login_manager.login_message_category = "warning"
 
 
 class Remote:
-    """A class used for instantiating a user's saved remote control configurations.
-        There should be 1 object of this class type per remote control.
-
+    """
+    A class used for instantiating a user's saved remote control configurations.
+    There should be 1 object of this class type per remote control.
     """
 
     def __init__(self, name, link='/remote'):
@@ -28,13 +28,15 @@ class Remote:
         self.link = link
 
 
+# pylint: disable=no-member
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column('user_id', db.Integer, primary_key=True)
-    username = db.Column('username', db.String(20), unique=True, index=True)
-    password = db.Column('password', db.String(10))
+    id = db.Column('user_id', db.Integer, primary_key=True, index=True)
+    username = db.Column('username', db.String(20), unique=True)
+    password = db.Column('password', db.String(20))
 
     def __init__(self, username, password):
+        # Note: The primary key (id) is auto-generated
         self.username = username
         self.password = password
 
@@ -50,6 +52,8 @@ class User(db.Model):
     def get_id(self):
         """This class attrubute holds the user account's ID"""
         return str(self.id)
+# pylint: enable=no-member
+
 
 @login_manager.user_loader
 def load_user(user_id):
