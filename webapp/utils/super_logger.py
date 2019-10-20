00602b598d0b4336a732f83e28c7ebf27065a8b9
Tejas Shah <tejashah88@gmail.com>
2019-10-20 14:26:53 -0700

    added holy diwali mode for logs

--- webapp/utils/super_logger.py
+++ webapp/utils/super_logger.py
@@ -15,7 +15,7 @@ Example:
 
 import logging
 import colorama
-from .singleton import Singleton
+from colorama import Fore, Style
 
 class SuperLogger:
     """
@@ -79,81 +79,75 @@ class SuperLogger:
         """
         self._logger.setLevel(level)
 
-    def debug(self, tag, msg):
+    def debug(self, msg):
         """
         Prints a message in the 'debug' channel. This should be used for detailed logging of the
         internals of a specific module or script such as the number of bytes sent via the camera
         module or how much time was taken to handle an HTTP request.
         """
         if self.initialized:
-            final_msg = f'[{tag}]: {msg}'
             if self._use_color:
-                pass
+                msg = Style.NORMAL + Fore.GREEN + msg + Style.RESET_ALL
 
-            self._logger.debug(final_msg)
+            self._logger.debug(msg)
 
-    def info(self, tag, msg):
+    def info(self, msg):
         """
         Prints a message in the 'info' channel. This should be used for general events or
         notes that the user should know but it isn't a priority, such as what's the port
         and host address of the Flask server or which web page did someone just access.
         """
         if self.initialized:
-            final_msg = f'[{tag}]: {msg}'
             if self._use_color:
-                pass
+                msg = Style.DIM + Fore.CYAN + msg + Style.RESET_ALL
 
-            self._logger.info(final_msg)
+            self._logger.info(msg)
 
-    def warning(self, tag, msg):
+    def warning(self, msg):
         """
         Prints a message in the 'warning' channel. This should be used when an event in the web app
         has happened that the user should pay attention to, such as a missing config file or if
         a sensor isn't configured correctly (that won't greatly affect the performance of said sensor).
         """
         if self.initialized:
-            final_msg = f'[{tag}]: {msg}'
             if self._use_color:
-                pass
+                msg = Style.NORMAL + Fore.YELLOW + msg + Style.RESET_ALL
 
-            self._logger.warning(final_msg)
+            self._logger.warning(msg)
 
-    def error(self, tag, msg):
+    def error(self, msg):
         """
         Prints a message in the 'error' channel. This should be used when an error has occurred
         that could make the web app unstable, such as a missing sensor attached, or if a 500 server
         error occurred internally.
         """
         if self.initialized:
-            final_msg = f'[{tag}]: {msg}'
             if self._use_color:
-                pass
+                msg = Style.NORMAL + Fore.RED + msg + Style.RESET_ALL
 
-            self._logger.error(final_msg)
+            self._logger.error(msg)
 
-    def exception(self, tag, msg):
+    def exception(self, msg):
         """
         Prints a message in the 'exception' channel. This should be **ONLY** used when an exception has occurred.
         Exception info is added to the logging message. This function should only be called from an exception handler.
         """
         if self.initialized:
-            final_msg = f'[{tag}]: {msg}'
             if self._use_color:
-                pass
+                msg = Style.BRIGHT + Fore.RED + msg + Style.RESET_ALL
 
-            self._logger.exception(final_msg)
+            self._logger.exception(msg)
 
-    def critical(self, tag, msg):
+    def critical(self, msg):
         """
         Prints a message in the 'critical' channel. This should be used when a critical event has occurred
         that user **must** pay attention to it, such as if the camera module or web app crashed.
         """
         if self.initialized:
-            final_msg = f'[{tag}]: {msg}'
             if self._use_color:
-                pass
+                msg = Style.BRIGHT + Fore.MAGENTA + msg + Style.RESET_ALL
 
-            self._logger.critical(final_msg)
+            self._logger.critical(msg)
 
 # provide a default SuperLogger instance
 logger = SuperLogger()
